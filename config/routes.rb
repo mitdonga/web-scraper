aacad7faa535fcc255a4e2a1323894c23598b255
Mit Donga <meetdonga5344@gmail.com>
2022-07-06 12:35:53 +0530

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 require'sidekiq/web' 
 
 Rails.application.routes.draw do
-  # if Rails.env.development?
-  mount GraphiQL::Rails::Engine, at: "/try", graphql_path: "/"
-  # end
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/try", graphql_path: "/"
+  end
   post "/", to: "graphql#execute"
 
   mount Sidekiq::Web => '/sidekiq'
